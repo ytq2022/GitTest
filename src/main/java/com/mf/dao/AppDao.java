7d5ac4d227f5f9d68b9ede8324589e6f4cdf913f
eclipse_name <eclipse_email@126.com>
2022-06-25 20:40:25 +0800

    commit hot_fix

--- src/main/java/com/mf/dao/AppDao.java
+++ src/main/java/com/mf/dao/AppDao.java
@@ -4,6 +4,8 @@ public class AppDao {
 	public static void main(String[] args) {
 		System.out.println("--------left ------right---------");
 		System.out.println("--------left ----left-----------");
+		System.out.println("----------hot_fix---------------");
+		
 	}
 
 }
